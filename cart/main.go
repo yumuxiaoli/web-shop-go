411e2bbcc5fda0df3bd6307a947e30d2566bd654
yumu <2816083598@qq.com>
2023-10-09 14:41:41 +0800

    refactor:pushpin::更改依赖

--- cart/main.go
+++ cart/main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"fmt"
 
-	pb "github.com/yumuxiaoli/web-shop-go/cart/proto"
+	col2 "github.com/go-micro/plugins/v4/registry/consul"
+	"go-micro.dev/v4"
+	"go-micro.dev/v4/logger"
+	"go-micro.dev/v4/registry"
 
 	"github.com/jinzhu/gorm"
-	"github.com/micro/go-micro/util/log"
-	"github.com/micro/go-micro/v2"
-	"github.com/micro/go-micro/v2/registry"
-	sr2 "github.com/micro/go-micro/v2/registry/service"
 
+	ratelimit "github.com/go-micro/plugins/v4/wrapper/ratelimiter/uber"
+	op2 "github.com/go-micro/plugins/v4/wrapper/trace/opentracing"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	ratelimit "github.com/micro/go-plugins/wrapper/ratelimiter/uber/v2"
-	opentracing2 "github.com/micro/go-plugins/wrapper/trace/opentracing/v2"
 	"github.com/opentracing/opentracing-go"
 	"github.com/yumuxiaoli/common"
 	"github.com/yumuxiaoli/web-shop-go/cart/domain/repository"
 	"github.com/yumuxiaoli/web-shop-go/cart/domain/service"
 	"github.com/yumuxiaoli/web-shop-go/cart/handler"
+	pb "github.com/yumuxiaoli/web-shop-go/cart/proto"
 )
 
 var QPS = 100
@@ -27,10 +27,10 @@ func main() {
 	// 配置中心
 	consulConfig, err := common.GetConsulConfig("8.130.66.151", 8500, "micro/config")
 	if err != nil {
-		log.Error(err)
+		logger.Error(err)
 	}
 	// 注册中心
-	consul := sr2.NewRegistry(func(o *registry.Options) {
+	consul := col2.NewRegistry(func(o *registry.Options) {
 		o.Addrs = []string{
 			"8.130.66.151:8500",
 		}
@@ -38,7 +38,7 @@ func main() {
 	// 链路追踪
 	t, io, err := common.NewTracer("go.micro.service.cart", "localhost:6831")
 	if err != nil {
-		log.Error(err)
+		logger.Error(err)
 	}
 	defer io.Close()
 	opentracing.SetGlobalTracer(t)
@@ -47,7 +47,7 @@ func main() {
 	mysqlInfo := common.GetMysqlFromConsul(consulConfig, "mysql")
 	db, err := gorm.Open("mysql", mysqlInfo.User+":"+mysqlInfo.Pwd+"@tcp("+mysqlInfo.Host+":"+mysqlInfo.Port+")/"+mysqlInfo.Database+"?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
-		log.Error(err)
+		logger.Error(err)
 	}
 	defer db.Close()
 	// 禁止副表
@@ -56,7 +56,7 @@ func main() {
 	// 第一次初始化
 	err = repository.NewCartRepository(db).InitTable()
 	if err != nil {
-		log.Error(err)
+		logger.Error(err)
 	}
 
 	// Create service
@@ -67,7 +67,7 @@ func main() {
 		micro.Address("0.0.0.0:8087"),
 		micro.Registry(consul),
 		// 链路追踪
-		micro.WrapHandler(opentracing2.NewHandlerWrapper(opentracing.GlobalTracer())),
+		micro.WrapHandler(op2.NewHandlerWrapper(opentracing.GlobalTracer())),
 		// 添加限流
 		micro.WrapHandler(ratelimit.NewHandlerWrapper(QPS)),
 	)
@@ -77,8 +77,10 @@ func main() {
 	cartDataService := service.NewCartDataService(repository.NewCartRepository(db))
 
 	// Register handler
-	pb.RegisterCartHandler(srv.Server(), &handler.Cart{CartDataService: cartDataService})
-
+	err = pb.RegisterCartHandler(srv.Server(), &handler.Cart{CartDataService: cartDataService})
+	if err != nil {
+		logger.Error(err)
+	}
 	// Run service
 	if err := srv.Run(); err != nil {
 		fmt.Println(err)
